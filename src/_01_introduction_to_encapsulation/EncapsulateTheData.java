9907814378096956eb952d70694214368d51961c
11002084 <36611325+11002084@users.noreply.github.com>
2020-11-22 16:43:21 -0800

    11/22/2020
    Started Encapsulate the Data

--- src/_01_introduction_to_encapsulation/EncapsulateTheData.java
+++ src/_01_introduction_to_encapsulation/EncapsulateTheData.java
@@ -23,10 +23,54 @@ public class EncapsulateTheData {
 	//2. Create a new JUnit Test case and write tests to verify that 
 	//   the member variables' getters and setters are working
 	
-	int itemsReceived; //must not be negative. All negative arguments get set to 0.
-	float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
-	String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
-	Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
+	private int itemsReceived; //must not be negative. All negative arguments get set to 0.
+	private float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
+	private String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
+	private Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
+	
+	public int getItemsRecieved() {
+		return itemsReceived;
+	}
+	
+	public void setItemsRecieved(int itemsReceived) {
+		if(itemsReceived < 0) {
+			this.itemsReceived = 0;
+		} else {
+			this.itemsReceived = itemsReceived;
+		}
+	}
+	
+	public float getDegreesTurned() {
+		return degreesTurned;
+	}
+	
+	public void setDegreesTurned(float degreesTurned) throws IllegalArgumentException{
+		if(degreesTurned < 0.0 || degreesTurned > 360.0) {
+			throw new IllegalArgumentException();
+		} else {
+			this.degreesTurned = degreesTurned;
+		}
+	}
+	
+	public String getNomenclature() {
+		return nomenclature;
+	}
+	
+	public void setNomenclature(String nomenclature) {
+		if(nomenclature.equals("")) {
+			this.nomenclature = " ";
+		} else {
+			this.nomenclature = nomenclature;
+		}
+	}
+	
+	public Object getMemberObj() {
+		return memberObj;
+	}
+	
+	public void setMemberObj(Object obj) {
+		
+	}
 	
 	public static void main(String[] args) {
 		
